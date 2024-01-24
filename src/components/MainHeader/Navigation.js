adeb5e1c10e463fa17975deb0f23299fddd01f46
nabankur2000m <nabankur2000m@gmail.com>
2024-01-24 23:20:13 +0530

    Rules of Hooks

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -5,28 +5,27 @@ import classes from './Navigation.module.css';
 
 const Navigation = () => {
   const ctx = useContext(AuthContext);
+
   return (
-    
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={ctx.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-      
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={ctx.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
